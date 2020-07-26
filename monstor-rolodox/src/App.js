6e424e22824b0cf5cba675772c86ce8bc99247ec
im-hujjin <im_hemaraj@hotmail.com>
2020-07-25 20:46:02 -0400

    adding files for github pages

---
+++ monstor-rolodox/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+  }
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({ monsters: users}));
+  }
+
+  render() {
+    
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+    
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    
+    return (
+      <div className="App">
+      <h1>Monsters Rolodex</h1>
+      <SearchBox 
+        placeholder="search Monsters"
+        handleChange={e => this.setState({searchField: e.target.value})}
+      />
+      <CardList monsters={ filteredMonsters } />
+      </div>
+    );
+    
+  }
+}
+
+export default App;

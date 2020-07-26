af9de7b57d0d6ced8bd7887540f977d1723091fe
im-hujjin <im_hemaraj@hotmail.com>
2020-07-25 20:45:15 -0400

    Updates

---
+++ precache-manifest.83fbfbca811a7395efd690bf7ecaf44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c527142a14e3c3444b2cf371bc1c7b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4c5f7d3cd76627ca97e0",
+    "url": "/monster-rolodex/static/css/main.062d20b1.chunk.css"
+  },
+  {
+    "revision": "9a24923fa0e224760729",
+    "url": "/monster-rolodex/static/js/2.15e60ab4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.15e60ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c5f7d3cd76627ca97e0",
+    "url": "/monster-rolodex/static/js/main.1606c356.chunk.js"
+  },
+  {
+    "revision": "be158fc1f7b7e5f9ff18",
+    "url": "/monster-rolodex/static/js/runtime-main.42d65b70.js"
+  }
+]);
